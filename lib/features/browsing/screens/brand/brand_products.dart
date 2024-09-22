1bd6ff85458731852d37946af6e4a4b4b7492a2a
ivanyadrian <ivany.adrian067@gmail.com>
2024-09-22 12:05:06 +0200

    backup_21

--- lib/features/browsing/screens/brand/brand_products.dart
+++ lib/features/browsing/screens/brand/brand_products.dart
@@ -5,20 +5,21 @@ import 'package:szakdolgozat_app/common/widgets/products/sortable/sortable_produ
 import 'package:szakdolgozat_app/utils/constans/size.dart';
 
 class BrandProducts extends StatelessWidget {
-  const BrandProducts({super.key});
+  final String countyName;
+
+  const BrandProducts({super.key, required this.countyName});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TAppBar(title: Text('Countyname'), showBackArrow: true),
+    return Scaffold(
+      appBar: TAppBar(title: Text(countyName), showBackArrow: true), // Use the county name dynamically
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(TSize.defaultSpace),
+          padding: const EdgeInsets.all(TSize.defaultSpace),
           child: Column(
-            children: [
+            children: const [
               TBrandCard(showBorder: true),
               SizedBox(height: TSize.spaceBetweenSections),
-
               TSortableProducts(),
             ],
           ),
@@ -27,3 +28,4 @@ class BrandProducts extends StatelessWidget {
     );
   }
 }
+
