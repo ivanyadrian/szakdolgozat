0cabdaffd29e57814edf3388aa72714534346d91
ivanyadrian <ivany.adrian067@gmail.com>
2024-10-02 15:33:27 +0200

    backup_26 (favourite)

--- lib/utils/local_storage/storage_utility.dart
+++ lib/utils/local_storage/storage_utility.dart
@@ -2,6 +2,7 @@ import 'package:get_storage/get_storage.dart';
 
 class TLocalStorage {
   late final GetStorage _storage;
+  String _bucketName = ''; // Store the bucket name
 
   // Singleton instance
   static TLocalStorage? _instance;
@@ -10,42 +11,52 @@ class TLocalStorage {
 
   // Factory constructor to return the singleton instance
   factory TLocalStorage.instance() {
-    _instance ??= TLocalStorage._internal();  // Correct assignment of the instance
+    _instance ??= TLocalStorage._internal();
     return _instance!;
   }
 
-  // Initialize the storage with the provided bucket name
+  /// Initialize the storage with the provided bucket name
   static Future<void> init(String bucketName) async {
-    try {
-      print('Initializing GetStorage with bucket name: $bucketName');
-      await GetStorage.init(bucketName);
+    if (_instance == null) {
       _instance = TLocalStorage._internal();
-      _instance!._storage = GetStorage(bucketName);
-      print('GetStorage has been initialized successfully.');
-    } catch (e) {
-      print('Error during TLocalStorage initialization: $e');
-      rethrow;
+      try {
+        print('Initializing GetStorage with bucket name: $bucketName');
+        await GetStorage.init(bucketName);
+        _instance!._storage = GetStorage(bucketName);
+        _instance!._bucketName = bucketName; // Store the bucket name
+        print('GetStorage has been initialized successfully.');
+      } catch (e) {
+        print('Error during TLocalStorage initialization: $e');
+        rethrow; // Consider logging or handling the error appropriately
+      }
+    } else {
+      print('GetStorage is already initialized with bucket name: ${_instance!._bucketName}');
     }
   }
 
-
-  // Method to save data
+  /// Save data to local storage
   Future<void> saveData<T>(String key, T value) async {
     await _storage.write(key, value);
+    print('Saved data: $key = $value');
   }
 
-  // Method to read data
+  /// Read data from local storage
   T? readData<T>(String key) {
-    return _storage.read<T>(key);
+    final value = _storage.read<T>(key);
+    print('Read data: $key = $value');
+    return value;
   }
 
-  // Method to remove data by key
+  /// Remove data from local storage by key
   Future<void> removeData(String key) async {
     await _storage.remove(key);
+    print('Removed data: $key');
   }
 
-  // Method to clear all stored data
+  /// Clear all stored data
   Future<void> clearAll() async {
     await _storage.erase();
+    print('All data cleared from storage.');
   }
 }
+
